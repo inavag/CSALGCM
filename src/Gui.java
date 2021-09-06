05c7e30664deee70b87e38a4332d0ba89a60120c
Ina Evangelista <ina_evangelista@dlsu.edu.ph>
2021-09-07 07:43:51 +0800

    gui

--- src/Gui.java
+++ src/Gui.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
 
 /**
   * This class is responsible for GUI of this application. 
@@ -26,7 +28,7 @@ public class Gui extends JFrame{
         initChoice();
         setLayout();
 
-        setPreferredSize(new Dimension(500, 670));
+        setPreferredSize(new Dimension(740, 475));
         setTitle("Meal Planner");
         this.pack();
         this.setLocationRelativeTo(null); //makes the window open on the center of the screen
@@ -42,13 +44,18 @@ public class Gui extends JFrame{
     private void initTextArea() {
         textPanel = new JPanel(new BorderLayout());
         log = new JTextArea(20, 45);
-        log.setBorder(BorderFactory.createLineBorder(Color.black));
+        //log.setBorder(BorderFactory.createLineBorder(Color.black));
         log.setLineWrap(true);
         log.setEditable(false);
         log.setVisible(true);
         scroll = new JScrollPane(log);
         scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+
+        Border border = new LineBorder(Color.BLACK, 3);
+
         textPanel.add(scroll, BorderLayout.CENTER);
+        textPanel.setBorder(border);
+        //textPanel.setPreferredSize(new Dimension(300,200));
     }
 
     /**
@@ -60,30 +67,61 @@ public class Gui extends JFrame{
         nInputs = new JTextField[4];
         
         nLabels[0] = new JLabel("Target Calories: ");
-        nInputs[0] = new JTextField(5);
+        nInputs[0] = new JTextField();
 
         nLabels[1] = new JLabel("Food: ");
-        nInputs[1] = new JTextField(7);
+        nInputs[1] = new JTextField();
 
         nLabels[2] = new JLabel("Grams (g): ");
-        nInputs[2] = new JTextField(4);
+        nInputs[2] = new JTextField();
 
         nLabels[3] = new JLabel("Calories: ");
-        nInputs[3] = new JTextField(5);
+        nInputs[3] = new JTextField();
 
         //creation of buttons in the application
+        
+        Font font1 = new Font("Courier",Font.BOLD, 15);
+        Border border = new LineBorder(Color.BLACK, 3);
+
+        nLabels[0].setFont(font1);
+        nInputs[0].setBorder(border);
+        nInputs[0].setPreferredSize(new Dimension(145,35));
+
+        nLabels[1].setFont(font1);
+        nInputs[1].setBorder(border);
+        nInputs[1].setPreferredSize(new Dimension(65,35));
+
+        nLabels[2].setFont(font1);
+        nInputs[2].setBorder(border);
+        nInputs[2].setPreferredSize(new Dimension(65,35));
+
+        nLabels[3].setFont(font1);
+        nInputs[3].setBorder(border);
+        nInputs[3].setPreferredSize(new Dimension(65,35));
 
         set = new JButton("SET");
-        set.setPreferredSize(new Dimension(60, 20));
+        set.setFont(font1);
+        set.setBorder(border);
+        set.setBackground(Color.WHITE);
+        set.setPreferredSize(new Dimension(145, 35));
 
         add = new JButton("ADD");
-        add.setPreferredSize(new Dimension(60, 20));
+        add.setFont(font1);
+        add.setBorder(border);
+        add.setBackground(Color.WHITE);
+        add.setPreferredSize(new Dimension(145, 35));
 
         confirm = new JButton("CONFIRM");
-        confirm.setPreferredSize(new Dimension(100, 20));
+        confirm.setFont(font1);
+        confirm.setBorder(border);
+        confirm.setBackground(Color.WHITE);
+        confirm.setPreferredSize(new Dimension(145, 35));
 
         clear = new JButton("CLEAR");
-        clear.setPreferredSize(new Dimension(100, 20));
+        clear.setFont(font1);
+        clear.setBorder(border);
+        clear.setBackground(Color.WHITE);
+        clear.setPreferredSize(new Dimension(145, 35));
     }
 
     /**
@@ -99,6 +137,14 @@ public class Gui extends JFrame{
         greedy.setSelected(true);
         radioGrp.add(greedy);
         radioGrp.add(dynamic);
+
+        Font font1 = new Font("Courier",Font.BOLD, 15);
+
+        greedy.setBackground(Color.WHITE);
+        greedy.setFont(font1);
+
+        dynamic.setBackground(Color.WHITE);
+        dynamic.setFont(font1);
     }
     /**
      * This function sets the position of the elements in 
@@ -111,45 +157,45 @@ public class Gui extends JFrame{
 
         //Position the Target Calorie input
         spring.putConstraint(SpringLayout.NORTH, nLabels[0], 20, SpringLayout.NORTH, root);
-        spring.putConstraint(SpringLayout.WEST, nLabels[0], 10, SpringLayout.WEST, root);
-        spring.putConstraint(SpringLayout.NORTH, nInputs[0], 20, SpringLayout.NORTH, root);
-        spring.putConstraint(SpringLayout.WEST, nInputs[0], 110, SpringLayout.WEST, root);
+        spring.putConstraint(SpringLayout.WEST, nLabels[0], -155, SpringLayout.EAST, root);
+        spring.putConstraint(SpringLayout.NORTH, nInputs[0], 50, SpringLayout.NORTH, root);
+        spring.putConstraint(SpringLayout.WEST, nInputs[0], -170, SpringLayout.EAST, root);
 
         //Position the Food input
-        spring.putConstraint(SpringLayout.NORTH, nLabels[1], 60, SpringLayout.NORTH, root);
-        spring.putConstraint(SpringLayout.WEST, nLabels[1], 10, SpringLayout.WEST, root);
-        spring.putConstraint(SpringLayout.NORTH, nInputs[1], 60, SpringLayout.NORTH, root);
-        spring.putConstraint(SpringLayout.WEST, nInputs[1], 50, SpringLayout.WEST, root);
+        spring.putConstraint(SpringLayout.NORTH, nLabels[1], 184, SpringLayout.NORTH, root);
+        spring.putConstraint(SpringLayout.WEST, nLabels[1], -170, SpringLayout.EAST, root);
+        spring.putConstraint(SpringLayout.NORTH, nInputs[1], 179, SpringLayout.NORTH, root);
+        spring.putConstraint(SpringLayout.WEST, nInputs[1], -90, SpringLayout.EAST, root);
         //Position the Grams input
-        spring.putConstraint(SpringLayout.NORTH, nLabels[2], 60, SpringLayout.NORTH, root);
-        spring.putConstraint(SpringLayout.WEST, nLabels[2], 140, SpringLayout.WEST, root);
-        spring.putConstraint(SpringLayout.NORTH, nInputs[2], 60, SpringLayout.NORTH, root);
-        spring.putConstraint(SpringLayout.WEST, nInputs[2], 205, SpringLayout.WEST, root);
+        spring.putConstraint(SpringLayout.NORTH, nLabels[2], 229, SpringLayout.NORTH, root);
+        spring.putConstraint(SpringLayout.WEST, nLabels[2], -170, SpringLayout.EAST, root);
+        spring.putConstraint(SpringLayout.NORTH, nInputs[2], 224, SpringLayout.NORTH, root);
+        spring.putConstraint(SpringLayout.WEST, nInputs[2], -90, SpringLayout.EAST, root);
         //Position the calories input
-        spring.putConstraint(SpringLayout.NORTH, nLabels[3], 60, SpringLayout.NORTH, root);
-        spring.putConstraint(SpringLayout.WEST, nLabels[3], 260, SpringLayout.WEST, root);
-        spring.putConstraint(SpringLayout.NORTH, nInputs[3], 60, SpringLayout.NORTH, root);
-        spring.putConstraint(SpringLayout.WEST, nInputs[3], 315, SpringLayout.WEST, root);
+        spring.putConstraint(SpringLayout.NORTH, nLabels[3], 274, SpringLayout.NORTH, root);
+        spring.putConstraint(SpringLayout.WEST, nLabels[3], -170, SpringLayout.EAST, root);
+        spring.putConstraint(SpringLayout.NORTH, nInputs[3], 269, SpringLayout.NORTH, root);
+        spring.putConstraint(SpringLayout.WEST, nInputs[3], -90, SpringLayout.EAST, root);
         //Position the SET button
-        spring.putConstraint(SpringLayout.NORTH, set, 20, SpringLayout.NORTH, root);
-        spring.putConstraint(SpringLayout.WEST, set, 200, SpringLayout.WEST, root);
+        spring.putConstraint(SpringLayout.NORTH, set, 95, SpringLayout.NORTH, root);
+        spring.putConstraint(SpringLayout.WEST, set, -170, SpringLayout.EAST, root);
         //Position the ADD button
-        spring.putConstraint(SpringLayout.NORTH, add, 60, SpringLayout.NORTH, root);
-        spring.putConstraint(SpringLayout.WEST, add, 400, SpringLayout.WEST, root);
+        spring.putConstraint(SpringLayout.NORTH, add, 314, SpringLayout.NORTH, root);
+        spring.putConstraint(SpringLayout.WEST, add, -170, SpringLayout.EAST, root);
         //Position Radio Buttons
-        spring.putConstraint(SpringLayout.NORTH, greedy, 115, SpringLayout.NORTH, root);
-        spring.putConstraint(SpringLayout.WEST, greedy, 175, SpringLayout.WEST, root);
-        spring.putConstraint(SpringLayout.NORTH, dynamic, 135, SpringLayout.NORTH, root);
-        spring.putConstraint(SpringLayout.WEST, dynamic, 175, SpringLayout.WEST, root);
+        spring.putConstraint(SpringLayout.NORTH, greedy, 375, SpringLayout.NORTH, root);
+        spring.putConstraint(SpringLayout.WEST, greedy, 23, SpringLayout.WEST, root);
+        spring.putConstraint(SpringLayout.NORTH, dynamic, 375, SpringLayout.NORTH, root);
+        spring.putConstraint(SpringLayout.WEST, dynamic, 180, SpringLayout.WEST, root);
         //Position the CONFIRM button
-        spring.putConstraint(SpringLayout.NORTH, confirm, 175, SpringLayout.NORTH, root);
-        spring.putConstraint(SpringLayout.WEST, confirm, 200, SpringLayout.WEST, root);
+        spring.putConstraint(SpringLayout.NORTH, confirm, 375, SpringLayout.NORTH, root);
+        spring.putConstraint(SpringLayout.WEST, confirm, -170, SpringLayout.EAST, root);
         //Position the CLEAR button
-        spring.putConstraint(SpringLayout.NORTH, clear, 600, SpringLayout.NORTH, root);
-        spring.putConstraint(SpringLayout.WEST, clear, 200, SpringLayout.WEST, root);
+        spring.putConstraint(SpringLayout.NORTH, clear, 375, SpringLayout.NORTH, root);
+        spring.putConstraint(SpringLayout.WEST, clear, 380, SpringLayout.WEST, root);
         //Position the text panel
-        spring.putConstraint(SpringLayout.NORTH, textPanel, 270, SpringLayout.NORTH, root);
-        spring.putConstraint(SpringLayout.WEST, textPanel, 15, SpringLayout.WEST, root);
+        spring.putConstraint(SpringLayout.NORTH, textPanel, 20, SpringLayout.NORTH, root);
+        spring.putConstraint(SpringLayout.WEST, textPanel, 23, SpringLayout.WEST, root);
 
         //This loop adds all the labels and inputs on the main panel. 
 
@@ -157,6 +203,8 @@ public class Gui extends JFrame{
             root.add(nLabels[i]);
             root.add(nInputs[i]);
         }
+
+        root.setBackground(Color.WHITE);
 
         // This adds the remaining elements on the main panel. 
         root.add(set);
