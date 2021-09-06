d4493d06b40f0cf7d0c272c6dc315e21afbc7464
Carla Jeannine Cruzada <80302664+enigmaticarly@users.noreply.github.com>
2021-09-06 13:37:12 +0800

    Added internal documentation

--- src/FoodItem.java
+++ src/FoodItem.java
@@ -1,3 +1,11 @@
+/** 
+ * This class is responsible the creation of the object FoodItem, which
+ * will be used later on as type of Arraylist that stores the information 
+ * about an item. 
+ * 
+ */
+
+
 public class FoodItem {
 
     private String foodName;
@@ -10,16 +18,31 @@ public class FoodItem {
         this.calories = calories;
         this.ratio = (float) grams / calories;
     }
-    
+    /**
+     * This function gets the name of a food portion. 
+     * @return the string variable.  
+     */
     public String getFoodName() {
         return foodName;
     }
+    /**
+     * This function gets the number of grams of a food portion. 
+     * @return the integer variable.  
+     */
     public int getGrams() {
         return grams;
     }
+     /**
+     * This function gets the number of calories of a food portion. 
+     * @return the integer variable. 
+     */
     public int getCalories() {
         return calories;
     }
+    /**
+     * This function gets the ratio (Grams / calories) of a food portion.
+     * @return the float variable. 
+     */
     public float getRatio() {
         return ratio;
     }
